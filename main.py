8acc9e85532421baac64b81248e1e89ff8602da0
Douglas Wangome <ishdagnesh@gmail.com>
2023-03-16 10:40:23 +0300

    Complete final step

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from hashvalue import get_expected_hashvalue
+from installer import compare_hash_values
 
 def main():
   # Step 1: Get expected hash value
   expected_hash_value = get_expected_hashvalue()
   print(expected_hash_value)
 
+  # Step 2: Download the VLC installer and compares hash values
+  compare_hash_values(expected_hash_value)
 
 main()
